955ad225001a57157ee510f65b80f931de729dde
PJA <anne0103@outlook.com>
2019-08-18 22:17:54 +0800

    Add  update and delete

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -25,6 +25,31 @@ class PlayersController < ApplicationController
         @player = Player.find_by(id: params[:id])
     end
 
+    def edit
+        @player = Player.find_by(id: params[:id])
+    end
+
+    def update
+        @player = Player.find_by(id: params[:id])
+        
+        if @player.update(simple)
+            flash[:notice] = "Player修改完成"
+            redirect_to '/players'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @player = Player.find_by(id: params[:id])
+
+        @player.destroy
+            flash[:notice] = "Player刪除"
+            redirect_to '/players'
+
+
+    end
+
     private
     def simple
         params.require(:player).permit(:name, :age, :reason)
